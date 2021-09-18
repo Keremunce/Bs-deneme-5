81cc107db527cd548f009092c35495b569c3ae70
Kerem Unce <Keremunce@hotmail.com>
2021-09-18 11:36:34 +0300

    second

--- js/main.js
+++ js/main.js
@@ -2,23 +2,29 @@ $(window).scroll(function(){
     $('.navbar').toggleClass('scrolled', $(this).scrollTop()>50);
 });
 
-$('.owl-carousel').owlCarousel({
+// $('.owl-carousel').owlCarousel({
 
-    loop:true,
-    margin:10,
-    nav:true,
-    navText:[  '<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
-    navClass: ['owl-prev', 'owl-next'],
-    dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:2.5
-        }
-    }
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     navText:[  '<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+//     navClass: ['owl-prev', 'owl-next'],
+//     dots:false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:2
+//         },
+//         1000:{
+//             items:2.5
+//         }
+//     }
+// });
+
+let video = document.querySelector('.hero');
+window.addEventListener('scroll', function() {
+    let value =   .9   + window.scrollY / -600;
+    video.style.opacity = value;
 })
