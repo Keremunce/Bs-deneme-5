4614a7ebb2378df4ddbcab607c7aeab762ac38e6
Kerem Unce <Keremunce@hotmail.com>
2021-09-21 16:29:51 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -2,29 +2,12 @@ $(window).scroll(function(){
     $('.navbar').toggleClass('scrolled', $(this).scrollTop()>50);
 });
 
-// $('.owl-carousel').owlCarousel({
-
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     navText:[  '<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
-//     navClass: ['owl-prev', 'owl-next'],
-//     dots:false,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:2
-//         },
-//         1000:{
-//             items:2.5
-//         }
-//     }
-// });
+$(".navbar-toggler").click(function() {
+    $("nav").css("background", "#111111")
+});
 
 let video = document.querySelector('.hero');
 window.addEventListener('scroll', function() {
     let value =   .9   + window.scrollY / -600;
     video.style.opacity = value;
-})
+});
